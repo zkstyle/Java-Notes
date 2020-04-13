adbcd38720783b05a8645c8bd030fc48153f9297
elvis <712338173@qq.com>
2020-04-13 22:01:47 +0000

    Initial commit

---
+++ notes/Test.java
@@ -0,0 +1,19 @@
+import sun.applet.AppletClassLoader;
+
+import java.lang.reflect.Proxy;
+
+/**
+ * @BelongsProject: Java-Notes
+ * @BelongsPackage: PACKAGE_NAME
+ * @Author: elvis
+ * @CreateTime: 2020-04-13 14:58
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        //Proxy.newProxyInstance(Object obj,);
+        //InvocationHandler
+
+
+    }
+}
